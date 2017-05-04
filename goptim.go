12e13959244e084dfe9360d65c7621272721723a
acflorea <acflorea@gmail.com>
2017-05-04 21:45:25 +0300

    Start working on function evaluation generic framework

--- goptim.go
+++ goptim.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"fmt"
-	"github.com/acflorea/goptim/rand"
+	"errors"
 )
 
 func main() {
-	fmt.Println("Test")
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(rand.Float64(10, 15))
+	emptyMap := make(map[string]float64)
+	constant(emptyMap)
+
+	fmt.Println(Eval(constant, emptyMap))
+	fmt.Println(Eval(identity, map[string]float64{
+		"x": 1.2345,
+	}))
+
+}
+
+// Constant function
+func constant(_ map[string]float64) (float64, error) {
+	return 10, nil
+}
+
+// Constant function
+func identity(x map[string]float64) (float64, error) {
+	for _, value := range x {
+		return value, nil
 	}
+	return 0.0, errors.New("Not a single parameter map.")
+}
+
+// Evaluate the funtion f in the point p
+func Eval(f func(map[string]float64) (float64, error), p map[string]float64) (float64, error) {
+	return f(p)
 }
