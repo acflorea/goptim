b9934f0ad169e4ae8cf89067d4d29e9fb73a36e1
acflorea <acflorea@gmail.com>
2017-05-04 21:46:50 +0300

    Start working on function evaluation generic framework

--- goptim.go
+++ goptim.go
@@ -14,6 +14,9 @@ func main() {
 	fmt.Println(Eval(identity, map[string]float64{
 		"x": 1.2345,
 	}))
+	fmt.Println(Eval(x_square, map[string]float64{
+		"x": 2.5,
+	}))
 
 }
 
@@ -22,10 +25,18 @@ func constant(_ map[string]float64) (float64, error) {
 	return 10, nil
 }
 
-// Constant function
+// Identity function
 func identity(x map[string]float64) (float64, error) {
 	for _, value := range x {
 		return value, nil
+	}
+	return 0.0, errors.New("Not a single parameter map.")
+}
+
+// x^2 function
+func x_square(x map[string]float64) (float64, error) {
+	for _, value := range x {
+		return value * value, nil
 	}
 	return 0.0, errors.New("Not a single parameter map.")
 }
