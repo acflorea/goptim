98c95bd8e054ea4b0bbc15c5dce21db777e52aab
acflorea <acflorea@gmail.com>
2017-05-07 14:17:14 +0300

    Add Uniform random multipoint generator

--- rand/rand.go
+++ rand/rand.go
@@ -1,9 +1,51 @@
 package rand
 
-import "math/rand"
+import (
+	"math/rand"
+	"github.com/acflorea/goptim/functions"
+	"math"
+)
 
 // Generates a uniform random value between a and b
 func Float64(a, b float64) (float64, float64) {
 	original := rand.Float64()
 	return original, a + (b-a)*original
+}
+
+// One dimensional restriction [lowerBound, upperBound)
+type Range struct {
+	LowerBound, UpperBound float64
+}
+
+// A multipoint generator structure
+type MultiPointGenerator struct {
+	// Number of dimensions
+	DimensionsNo int
+	// Optional restrictions on each dimension
+	// Restrictions are considered in the order they are defined (1st restriction applies to 1st dimension etc)
+	Restrictions []Range
+	// How many points to generate
+	PointsNo int
+}
+
+// Generates g.PointsNo.
+// Each point is a collection of g.DimensionsNo uniform random values bounded to g.Restrictions
+func (g *MultiPointGenerator) RandomUniform() (points []functions.MultidimensionalPoint) {
+
+	points = make([]functions.MultidimensionalPoint, g.PointsNo)
+
+	for pIdx := 0; pIdx < g.PointsNo; pIdx++ {
+		values := make([]float64, g.DimensionsNo)
+		for dimIdx := 0; dimIdx < g.DimensionsNo; dimIdx++ {
+			lowerBound, upperBound := -math.MaxFloat32, math.MaxFloat32
+			if (len(g.Restrictions) > dimIdx) {
+				lowerBound = g.Restrictions[dimIdx].LowerBound
+				upperBound = g.Restrictions[dimIdx].UpperBound
+			}
+			_, values[dimIdx] = Float64(lowerBound, upperBound)
+		}
+		points[pIdx] = functions.MultidimensionalPoint{Values: values}
+	}
+
+	return points
 }
--- rand_test/rand_test.go
+++ rand_test/rand_test.go
@@ -3,16 +3,46 @@ package rand_test
 import (
 	"testing"
 	"github.com/acflorea/goptim/rand"
+	"fmt"
 )
 
-func TestFloat64(t *testing.T) {
+func Test_Float64(t *testing.T) {
 
 	a := 10.0
 	b := 20.0
 	for i := 0; i < 10; i++ {
 		_, r := rand.Float64(a, b)
 		if (r < a || r >= b) {
-			t.Error("Invelid number generated")
+			t.Error("Invalid number generated")
+		}
+	}
+}
+
+func Test_RandomUniformPointsGenerator(t *testing.T) {
+
+	generator := rand.MultiPointGenerator{
+		DimensionsNo: 2,
+		PointsNo:     10,
+		Restrictions: []rand.Range{
+			{-10, 0},
+			{0, 10},
+		},
+	}
+	generatedPoints := generator.RandomUniform()
+
+	if (len(generatedPoints) != 10) {
+		msg := fmt.Sprintf("Error generating points. "+
+			"Expected (%i) but got (%i).", 10, len(generatedPoints))
+		t.Error(msg)
+	}
+
+	for pIdx := 0; pIdx < len(generatedPoints); pIdx++ {
+		x := generatedPoints[pIdx].Values[0]
+		y := generatedPoints[pIdx].Values[1]
+		if x < -10 || x >= 0 || y < 0 || y >= 10 {
+			msg := fmt.Sprintf("Error generating points. Coordinate out of bounds "+
+				"(x, y) = (%f, %f)", x, y)
+			t.Error(msg)
 		}
 	}
 }
