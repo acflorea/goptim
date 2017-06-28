fa02d87d99ba1ceb8133e7655370c657decc1da5
acflorea <acflorea@gmail.com>
2017-06-28 21:40:45 +0300

    Map of functions by name

---
+++ functions/list.go
@@ -0,0 +1,11 @@
+package functions
+
+// Map with functions by name
+var Functions = map[string]NumericalFunction{
+	"F_x_square":   F_x_square,
+	"F_constant":   F_constant,
+	"F_identity":   F_identity,
+	"F_sombrero":   F_sombrero,
+	"LIBSVM_optim": LIBSVM_optim,
+	"SparkIt":      SparkIt,
+}
