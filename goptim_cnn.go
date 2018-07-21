d176e305820cd2bef1bc28d6c6a39b411a38d5bf
acflorea <acflorea@gmail.com>
2018-07-21 17:28:23 +0300

    drop *32 variables

--- goptim_cnn.go
+++ goptim_cnn.go
@@ -130,21 +130,21 @@ func Optimize_cnn(vargs map[string]interface{}) {
 	//1.75% due to main effect: X11
 
 	// fANOVA - list them here for brevity...
-	var x0 float32 = 7.40
-	var x1 float32 = 11.85
-	var x2 float32 = 0.51
-	var x3 float32 = 0.79
-	var x4 float32 = 1.62
-	var x5 float32 = 0.73
-	var x6 float32 = 2.26
-	var x7 float32 = 1.26
-	var x8 float32 = 26.28
-	var x9 float32 = 0.87
-	var x10 float32 = 3.22
-	var x11 float32 = 1.75
+	var x8 = 26.28 // neurons_1
+	var x1 = 11.85 // full_layers 1...4 !!!
+	var x0 = 7.40  // conv_layers  3...6 !!!
+	var x10 = 3.22 // neurons_3 5...2048
+	var x6 = 2.26  // maps_5 8...512
+	var x11 = 1.75 // neurons_4 5...2048
+	var x4 = 1.62  // maps_3 8...512
+	var x7 = 1.26  // maps_6 8...512
+	var x9 = 0.87  // neurons_2 5...2048
+	var x3 = 0.79  // maps_2 5...2048
+	var x5 = 0.73  // maps_4 5...2048
+	var x2 = 0.51  // maps_1 5...2048
 
 	// conv_layers, full_layers, maps_1, maps_2, maps_3, maps_4, maps_5, maps_6, [neurons_1], neurons_2, neurons_3, neurons_4
-	var probabilityToChange = []float32{x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11}
+	var probabilityToChange = []float64{x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11}
 
 	core.Optimize(
 		noOfExperiments,
